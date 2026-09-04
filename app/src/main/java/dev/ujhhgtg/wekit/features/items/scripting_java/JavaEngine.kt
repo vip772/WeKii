083b997b348b887db3db1ab3a025d24f3ce7eb27
vip772 <vip772e@gmail.com>
2026-09-04 15:26:26 +0000

    对齐脚本 PluginCallBack 公共 API

--- app/src/main/java/dev/ujhhgtg/wekit/features/items/scripting_java/JavaEngine.kt
+++ app/src/main/java/dev/ujhhgtg/wekit/features/items/scripting_java/JavaEngine.kt
@@ -312,9 +312,9 @@ object JavaEngine {
             importClass("me.hd.wauxv.data.bean.info.FriendInfo")
             importClass("me.hd.wauxv.data.bean.info.GroupInfo")
             importClass("me.hd.wauxv.data.bean.PayMsgBean")
-            importClass("MODULE.dev.ujhhgtg.wekit.features.items.scripting_java.PluginCallBack")
-            importClass("MODULE.dev.ujhhgtg.wekit.features.items.scripting_java.PluginCallBack.HttpCallback")
-            importClass("MODULE.dev.ujhhgtg.wekit.features.items.scripting_java.PluginCallBack.DownloadCallback")
+            importClass("MODULE.me.hd.wauxv.plugin.api.callback.PluginCallBack")
+            importClass("MODULE.me.hd.wauxv.plugin.api.callback.PluginCallBack.HttpCallback")
+            importClass("MODULE.me.hd.wauxv.plugin.api.callback.PluginCallBack.DownloadCallback")
             importPackage("dev.ujhhgtg.wekit.features.api.core")
             importPackage("dev.ujhhgtg.wekit.features.api.ui")
             // ===== Host Info =====
